195b351745736223f44da38c5aa1d0f8c03e33a3
201103649 Aulia Berliana <auliaaberliana@gmail.com>
2023-07-05 14:40:35 +0700

    loginC

---
+++ app/src/main/java/com/auliaberliana201103649/login/ForexRootModel.java
@@ -0,0 +1,21 @@
+package com.auliaberliana201103649.login;
+
+import com.google.gson.annotations.SerializedName;
+
+public class ForexRootModel
+{
+
+        @SerializedName("rates")
+        private ForexRatesModel ratesModel;
+
+        public ForexRootModel() {}
+
+        public ForexRatesModel getRatesModel() {
+            return ratesModel;
+        }
+
+        public void setRatesModel(ForexRatesModel ratesModel) {
+            this.ratesModel = ratesModel;
+        }
+
+}
--- app/src/main/java/com/auliaberliana201103649/login/MenuActivity.java
+++ app/src/main/java/com/auliaberliana201103649/login/MenuActivity.java
@@ -9,8 +9,8 @@ import android.widget.Button;
 
 public class MenuActivity extends AppCompatActivity
 {
-    private Button _tampilMahasiswaButton;
-    private Intent _tampilMahasiswaIntent;
+    private Button _tampilMahasiswaButton, _tampilForexButton;
+    private Intent _tampilMahasiswaIntent, _tampilForexIntent;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -19,18 +19,32 @@ public class MenuActivity extends AppCompatActivity
         setContentView(R.layout.activity_menu);
 
         initTampilMahasiswaButton();
+        initTampilForexButton();
     }
 
     private void  initTampilMahasiswaButton()
     {
         _tampilMahasiswaButton = (Button) findViewById(R.id.tampilMahasiswaButton);
-
         _tampilMahasiswaButton.setOnClickListener(new View.OnClickListener()
         {
             @Override
             public void onClick(View v) {
                 _tampilMahasiswaIntent = new Intent(getApplicationContext(), TampilMahasiswaActivity.class);
                 startActivity(_tampilMahasiswaIntent);
+            }
+        });
+    }
+
+    private void initTampilForexButton()
+    {
+        _tampilForexButton = (Button) findViewById(R.id.tampilForexButton);
+        _tampilForexButton.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                _tampilForexIntent = new Intent(getApplicationContext(), ForexMainActivity.class);
+                startActivity(_tampilForexIntent);
             }
         });
     }
