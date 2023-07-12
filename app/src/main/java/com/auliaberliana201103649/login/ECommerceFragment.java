bb09cfff13c0b1574b5b16be9d513150c6974f7c
201103649 Aulia Berliana <auliaaberliana@gmail.com>
2023-07-12 14:44:53 +0700

    tabb

--- app/src/main/java/com/auliaberliana201103649/login/ECommerceFragment.java
+++ app/src/main/java/com/auliaberliana201103649/login/ECommerceFragment.java
@@ -70,7 +70,7 @@ public class ECommerceFragment extends Fragment {
         View view = inflater.inflate(R.layout.fragment_e_commerce, container, false);
 
         _webView = (WebView) view.findViewById(R.id.eCommerceWebView);
-        _webView.loadUrl("https://shopee.co.id");
+        _webView.loadUrl("https://tokopedia.co.id");
 
         WebSettings settings = _webView.getSettings();
         settings.setJavaScriptEnabled(true);
